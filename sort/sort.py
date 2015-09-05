35a9ba642468e91a04478769ad11475dff48b559
Boxiang Sun <daetalusun@gmail.com>
2015-09-05 20:30:13 +0800

    add some sorting stub

--- sort/sort.py
+++ sort/sort.py
@@ -4,4 +4,17 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+def merge(list_a, list_b):
+    pass
 
+def mergeSort(arr):
+    pass
+
+def selectSort(arr):
+    pass
+
+def insertionSort(arr):
+    pass
+
+def quickSort(arr):
+    pass
