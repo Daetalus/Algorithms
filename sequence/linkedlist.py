ed557d01a8e87527a2e132e9928545e0a4fcc565
Boxiang Sun <daetalusun@gmail.com>
2015-09-07 20:44:15 +0800

    add basic implementation of linked list

--- sequence/linkedlist.py
+++ sequence/linkedlist.py
@@ -6,11 +6,26 @@ from __future__ import print_function
 
 
 class Node(object):
-    def __init__(self, data, next):
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
+        self.size = 0
 
 
 class LinkedList(object):
     def __init__(self):
         self.head = None
+
+    def __len__(self):
+        return self.size
+
+    def insert(self, data):
+        self.head = Node(data, self.head)
+        self.size += 1
+
+    def append(self, data):
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = Node(data)
+        self.size += 1
