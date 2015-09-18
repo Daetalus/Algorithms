0ce3fc297f4add5d48a7364badefb28119d63809
Boxiang Sun <daetalusun@gmail.com>
2015-09-18 10:23:33 +0800

    quick sort implementation, reimplement the Algorithm 4th edition, Java version

--- sort/sort.py
+++ sort/sort.py
@@ -4,17 +4,51 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+
 def merge(list_a, list_b):
     pass
+
 
 def mergeSort(arr):
     pass
 
+
 def selectSort(arr):
     pass
+
 
 def insertionSort(arr):
     pass
 
-def quickSort(arr):
-    pass
+
+def partition(a, low, high):
+    start = low + 1
+    end = high
+    pivotItem = a[low]
+    while True:
+        while a[start] < pivotItem:
+            start += 1
+            if (start == high):
+                break
+        while a[end] > pivotItem:
+            end -= 1
+            if (end <= low):
+                break
+        if (start >= end):
+            break
+        a[start] = a[end]
+
+    a[low] = a[end]
+    return end
+
+
+def _quickSort(a, low, high):
+    if high <= low:
+        return
+    pivot = partition(a, low, high)
+    _quickSort(a, low, pivot - 1)
+    _quickSort(a, pivot + 1, high)
+
+
+def quickSort(a):
+    _quickSort(a, 0, len(a) - 1)
