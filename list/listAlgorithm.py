eb2c19cf346d8663ab51e3c3c478bae87e3f5532
Boxiang Sun <daetalusun@gmail.com>
2015-10-20 17:01:13 +0800

    add basic linked list algorithm

---
+++ list/listAlgorithm.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def removeElements(head, val):
+    dummy = ListNode(0)
+    dummy.next = head
+    cur = head
+    last = dummy
+    while cur:
+        if cur.val == val:
+            last.next = cur.next
+        else:
+            last = cur
+        cur = cur.next
+    return dummy.next
+
+
+# Very important
+def swapPairs(head):
+    dummy = ListNode(0)
+    dummy.next = head
+    cur = dummy
+
+    while cur.next and cur.next.next:
+        next = cur.next.next.next
+        first = cur.next
+        second = cur.next.next
+        cur.next = second
+        cur.next.next = first
+        cur = cur.next.next
+        cur.next = next
+
+    return dummy.next
